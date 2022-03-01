ef8534c66e67535f8684d9e44d29c2c849e99525
Baranga69 <69077312+Baranga69@users.noreply.github.com>
2022-03-01 14:19:21 +0300

    Initial commit

---
+++ multi_column_ocr.py
@@ -0,0 +1,10 @@
+# importing the neccessary packages
+from sklearn.cluster import AgglomerativeClustering
+from pytesseract import Output
+from tabulate import tabulate
+import pandas as pd
+import numpy as np
+import pytesseract
+import argparse
+import imutils
+import cv2
